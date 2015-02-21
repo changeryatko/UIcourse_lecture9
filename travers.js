9415422e80df282cdfe245c8b2cc4559256f9fa5
Yaroslav Minyaylo <changer.slava@gmail.com>
2015-02-21 10:18:38 +0200

    Added Travers function.

---
+++ travers.js
@@ -0,0 +1,14 @@
+function Travers(pattern) {
+  var bodyEls = document.getElementsByTagName("body");
+  var match1 = /^[#][a-zA-Z0-9]+/g,
+      match2 = /^[.][a-zA-Z0-9]+/g,
+      match3 = /^[#][a-zA-Z0-9]+\s[.][a-zA-Z0-9]+/g,
+      match4 = /^[a-z0-9]+[.][a-zA-Z0-9]+/g;
+
+  // if Regexp pattern #id
+
+  // if Regexp pattern #class
+
+  // if Regexp pattern .class elements in #id 
+
+}
